c02d689a4e3278024bf28ff8527b5dd9fc084b26
Jhenn <jhennyfer.zarate@unmsm.edu.pe>
2024-10-10 23:57:40 -0500

    projects section updated

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import NavBar from "./components/NavBar.vue";
 import Presentation from "./components/Presentation.vue";
 import Skills from "./components/Skills.vue";
+import Projects from "./components/Projects.vue"
 </script>
 
 <template>
   <NavBar />
   <Presentation />
   <Skills />
+  <Projects />
 </template>
 
 <style scoped></style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex max-w-[1400px] mx-auto mt-8 justify-between">
+        <div class="projects w-[1400px] h-auto rounded-[40px] p-16">
+            <h1 class="text-4xl font-bold text-white text-center font-karma">Projects</h1>
+            <p class="text-xl font-bold text-white font-karma text-center mt-5">Coming soon...</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style scoped>
+.projects {
+    background-image: linear-gradient(to bottom left, #6a73db, #143375);
+}
+</style>
